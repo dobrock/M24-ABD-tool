abac32cc10960ddba70a8e08eb140fd8fd89efee
dobrock <service@motorsport24.de>
2025-05-12 22:33:59 +0200

    Bereinigte App-Komponente mit VorgangsTest Integration

--- src/App.tsx
+++ src/App.tsx
@@ -255,38 +255,6 @@ export default function App() {
           </div>
         </section>
 
-import React, { useEffect, useState } from 'react';
-
-function App() {
-  const [vorgaenge, setVorgaenge] = useState<any[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/api/vorgaenge')
-      .then(res => res.json())
-      .then(data => setVorgaenge(data))
-      .catch(err => console.error('Fehler beim Laden:', err));
-  }, []);
-
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Vorgangsverwaltung (Test API Call)</h1>
-      {vorgaenge.length === 0 ? (
-        <p>Keine Vorgänge vorhanden.</p>
-      ) : (
-        <ul>
-          {vorgaenge.map(vorgang => (
-            <li key={vorgang.id}>
-              {vorgang.kundename} - {vorgang.mrn} ({vorgang.erstelldatum})
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
-
-export default App;
-
         {/* Submit */}
         <div className="mt-12 text-center">
           <button
@@ -297,6 +265,11 @@ export default App;
           </button>
         </div>
       </div>
+
+      <div className="mt-12">
+      <VorgangsTest />
+      </div>
+
     </div>
   );
 }
