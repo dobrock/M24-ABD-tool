474839ec77272074261f9798983a5f7a56966413
Daniel Schwab <service@motorsport24.de>
2025-05-13 21:41:10 +0200

    Fix: Cleanup alte Vite Config und Korrektur static copy Plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
@@ -9,9 +9,9 @@ export default defineConfig({
       targets: [
         {
           src: 'public/_redirects',
-          dest: '.'
-        }
-      ]
+          dest: '.',
+        },
+      ],
     }),
   ],
 });
